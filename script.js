57d425951ef0347871b38a1f0da4330afbecc07e
Vitor Henrique <vitor-henrique1996@hotmail.com>
2024-04-08 14:03:01 -0300

    arredondamento, troca de ponto por virgula e localização

--- script.js
+++ script.js
@@ -6,6 +6,5 @@ n1 = n1.toFixed(2);
 // para trocar virgula por ponto
 n1.toFixed(2).replace('.', ',');
 
-// toLaocaleString() é uma função que formata o número de acordo com a localidade
+// toLocaleString() é uma função que formata o número de acordo com a localidade
 const valor = n1.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-console.log(valor);
